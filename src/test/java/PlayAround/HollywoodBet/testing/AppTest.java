5c16fa3676c52186667ad59b190cf8d521a72a5f
Masakhane Matanzima <sihle.matanzima@gmail.com>
2020-10-11 22:59:07 +0200

    Extent Report

--- src/test/java/PlayAround/HollywoodBet/testing/AppTest.java
+++ src/test/java/PlayAround/HollywoodBet/testing/AppTest.java
@@ -1,5 +1,6 @@
 package PlayAround.HollywoodBet.testing;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
@@ -9,16 +10,22 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import Utilities.ExtentReport;
+
 public class AppTest extends Base {
+	
 
 	@BeforeMethod
-	public void testSetup() {
+	public void testSetup(Method test) {
 		System.out.println("Test setup");
+		ExtentReport.createTest(test.getName());
 	}
 
 	@Test(dataProvider = "getTeams")
-	public void test(List<Hashtable> items) {
-		System.out.println("Home:"+items.get(0).get("Home").toString());
+	public void testTrial(List<Hashtable> items) {
+		System.out.println("Home:" + items.get(0).get("Home").toString());
+		System.out.println("directory"+directory);
+		
 	}
 
 	@AfterMethod
--- src/test/java/PlayAround/HollywoodBet/testing/Base.java
+++ src/test/java/PlayAround/HollywoodBet/testing/Base.java
@@ -1,18 +1,31 @@
 package PlayAround.HollywoodBet.testing;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import Utilities.ExtentReport;
+
 public class Base {
+	public static String directory;
+
+	public Base() {
+		
+	}
+
 	@BeforeSuite
 	public void setup() {
+		directory = System.getProperty("user.dir");	
+		ExtentReport.createExtentReports();
 		System.out.println("Setup");
 	}
-	
+
 	@AfterSuite
 	public void teardown() {
+		ExtentReport.closeReport();
 		System.out.println("Tear down");
 	}
 }
-
